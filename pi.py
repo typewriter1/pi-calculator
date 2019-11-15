1f489319bfcd06472b0a937c444d5f2a43caf216
typewriter1 <40742741+typewriter1@users.noreply.github.com>
2019-11-15 12:15:01 +0000

    Run for ever with infinite loop

--- pi.py
+++ pi.py
@@ -1,9 +1,13 @@
 from __future__ import print_function
-from future import division
+from __future__ import division
 import random, math
 from decimal import Decimal
+
 noInside = 0
-for n in range(1, 10000000):
+n = 0
+
+while True:
+    n += 1
     if (random.random() ** 2 + random.random() ** 2) ** 0.5 < 1.0:
         noInside += 1
     print((Decimal(noInside) / Decimal(n)) * 4, " after " + str(n) +" iterations")
